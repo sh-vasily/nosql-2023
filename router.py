01a30bc741dfa4aba16e8203c395dffbe8cbd653
sh-vasily <vasia.punk.rok@gmail.com>
2023-10-06 23:56:32 +0300

    add repository

--- router.py
+++ router.py
@@ -2,64 +2,54 @@ from typing import Any
 
 from bson import ObjectId
 from fastapi import APIRouter, status, Depends
-from motor.motor_asyncio import AsyncIOMotorCollection
 from starlette.responses import Response
 
-from db import get_db_collection
+from repository import Repository
 from student import Student, UpdateStudentModel
 
 router = APIRouter()
 
-students: list[Student] = []
-
-
-def map_student(student: Any) -> Student:
-    return Student(id=str(student['_id']), name=student['name'], age=student['age'])
-
-
-def get_filter(id: str) -> dict:
-    return {'_id': ObjectId(id)}
-
 
 @router.get("/")
-async def get_all_student(db_collection: AsyncIOMotorCollection = Depends(get_db_collection)) -> list[Student]:
-    db_students = []
-    async for student in db_collection.find():
-        db_students.append(map_student(student))
-    return db_students
+async def get_all_student(repository: Repository = Depends(Repository.get_instance)) -> list[Student]:
+    return await repository.get_all()
 
 
 @router.get("/{student_id}", response_model=Student)
-async def get_by_id(student_id: str, db_collection: AsyncIOMotorCollection = Depends(get_db_collection)) -> Any:
+async def get_by_id(student_id: str,
+                    repository: Repository = Depends(Repository.get_instance)) -> Any:
     if not ObjectId.is_valid(student_id):
         return Response(status_code=status.HTTP_400_BAD_REQUEST)
-    db_student = await db_collection.find_one(get_filter(student_id))
-    if db_student is None:
+    student = await repository.get_by_id(student_id)
+    if student is None:
         return Response(status_code=status.HTTP_404_NOT_FOUND)
-    return map_student(db_student)
+    return student
 
 
 @router.post("/")
-async def add_student(student: UpdateStudentModel, db_collection: AsyncIOMotorCollection = Depends(get_db_collection)) -> str:
-    insert_result = await db_collection.insert_one(dict(student))
-    return str(insert_result.inserted_id)
+async def add_student(student: UpdateStudentModel,
+                      repository: Repository = Depends(Repository.get_instance)) -> str:
+    return await repository.create(student)
 
 
 @router.delete("/{student_id}")
-async def remove_student(student_id: str, db_collection: AsyncIOMotorCollection = Depends(get_db_collection)) -> Response:
+async def remove_student(student_id: str,
+                         repository: Repository = Depends(Repository.get_instance)) -> Response:
     if not ObjectId.is_valid(student_id):
         return Response(status_code=status.HTTP_400_BAD_REQUEST)
-    db_student = await db_collection.find_one_and_delete(get_filter(student_id))
-    if db_student is None:
+    student = await repository.delete(student_id)
+    if student is None:
         return Response(status_code=status.HTTP_404_NOT_FOUND)
     return Response()
 
 
 @router.put("/{student_id}", response_model=Student)
-async def update_student(student_id: str, student_model: UpdateStudentModel, db_collection: AsyncIOMotorCollection = Depends(get_db_collection)) -> Any:
+async def update_student(student_id: str,
+                         student_model: UpdateStudentModel,
+                         repository: Repository = Depends(Repository.get_instance)) -> Any:
     if not ObjectId.is_valid(student_id):
         return Response(status_code=status.HTTP_400_BAD_REQUEST)
-    student = await db_collection.find_one_and_replace(get_filter(student_id), dict(student_model))
+    student = await repository.update(student_id, student_model)
     if student is None:
         return Response(status_code=status.HTTP_404_NOT_FOUND)
-    return map_student(student)
+    return student
---
+++ utils.py
@@ -0,0 +1,13 @@
+from typing import Any
+
+from bson import ObjectId
+
+from student import Student
+
+
+def map_student(student: Any) -> Student:
+    return Student(id=str(student['_id']), name=student['name'], age=student['age'])
+
+
+def get_filter(id: str) -> dict:
+    return {'_id': ObjectId(id)}
