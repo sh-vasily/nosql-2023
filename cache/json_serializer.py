4b0739c87d109b1f324eae629414f49f43d026ca
Василий Щербаков <basil-scherbakov@ya.ru>
2023-10-20 15:47:17 +0300

    universal serializing

--- cache/json_serializer.py
+++ cache/json_serializer.py
@@ -7,8 +7,8 @@ class JsonSerializer(object):
     def serialize(self, key, value):
         if isinstance(value, str):
             return value, 1
-        if isinstance(value, Student):
-            return json.dumps(dict(value)), 2
+        if type(value).__module__ != "__builtin__":
+            value = dict(value)
         return json.dumps(value), 2
 
     def deserialize(self, key, value, flags):
