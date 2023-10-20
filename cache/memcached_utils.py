beb10e4f600f3a492b31dca6667dedadab58192b
Василий Щербаков <basil-scherbakov@ya.ru>
2023-10-20 16:04:30 +0300

    distributed cache

--- cache/memcached_utils.py
+++ cache/memcached_utils.py
@@ -1,3 +1,4 @@
+from pymemcache import HashClient
 from pymemcache.client.base import Client
 import os
 
@@ -14,7 +15,7 @@ def connect_and_init_memcached():
     global memcached_client
     memcached_uri = os.getenv('MEMCACHED_URI')
     try:
-        memcached_client = Client(memcached_uri, serde=JsonSerializer())
+        memcached_client = HashClient(memcached_uri.split(','), serde=JsonSerializer())
         print(f'Connected to memcached with uri {memcached_uri}')
     except Exception as ex:
         print(f'Cant connect to memcached: {ex}')
